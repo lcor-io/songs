42d08d80e272da1517ef5a9170ee20a152ae8f6c
Léo Cornillon <leo.cornillon@yupwego.com>
2024-05-04 15:12:50 +0200

    feat add session middleware

--- src/main.go
+++ src/main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/compress"
 	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/session"
+	"github.com/google/uuid"
 
 	"github.com/joho/godotenv"
 
@@ -22,16 +25,36 @@ func main() {
 		panic(fmtErr)
 	}
 
-	spotify := services.Spotify(os.Getenv("SPOTIFY_CREDENTIALS"))
-
 	app := fiber.New(fiber.Config{
 		AppName: "songs",
 	})
+
+	spotify := services.Spotify(os.Getenv("SPOTIFY_CREDENTIALS"))
 
 	// Setup logger in dev
 	if os.Getenv("ENV") == "development" {
 		app.Use(logger.New(logger.Config{}))
 	}
+
+	// Use session middleware
+	store := session.New(session.Config{
+		Expiration:     time.Hour * 24 * 30,
+		KeyLookup:      "cookie:songs_session",
+		KeyGenerator:   uuid.New().String,
+		CookieSecure:   true,
+		CookieHTTPOnly: true,
+	})
+	app.Use(func(c fiber.Ctx) error {
+		sess, err := store.Get(c)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Error getting session")
+		}
+		fmt.Printf("Session ID: %s\n", sess.ID())
+		if err := sess.Save(); err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Error setting session")
+		}
+		return c.Next()
+	})
 
 	// Setup compression for incoming requests
 	app.Use(compress.New(compress.Config{
